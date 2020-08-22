fc3cdb90d91d6c70278ca455f62a0d4be323483e
Timur <wargerun@gmail.com>
2020-08-22 23:28:31 +0300

    On branch master  Changes to be committed: 	new file:   Tim.Manager.Db/Migrations/20200822194449_ModifiedCanBeNullable.Designer.cs 	new file:   Tim.Manager.Db/Migrations/20200822194449_ModifiedCanBeNullable.cs

---
+++ Tim.Manager.Db/Migrations/20200822194449_ModifiedCanBeNullable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tim.Manager.Db.Migrations
+{
+    public partial class ModifiedCanBeNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "PassItems",
+                type: "datetime2",
+                nullable: true,
+                defaultValueSql: "getdate()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "getdate()");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "PassItems",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "getdate()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true,
+                oldDefaultValueSql: "getdate()");
+        }
+    }
+}
