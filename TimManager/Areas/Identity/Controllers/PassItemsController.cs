5a728f16cdd5bd7c99dba5576f37d3140d98c67d
Timur <wargerun@gmail.com>
2020-08-23 19:56:45 +0300

    1. refactoring 2. update method repository 3. edit method view

--- TimManager/Areas/Identity/Controllers/PassItemsController.cs
+++ TimManager/Areas/Identity/Controllers/PassItemsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Tim.Manager.Db.Entities;
 using Tim.Manager.Db.Repositories.PassItems;
+using System;
 
 namespace TimManager.Areas.Identity.Controllers.PassManager
 {
@@ -62,9 +63,9 @@ namespace TimManager.Areas.Identity.Controllers.PassManager
 
                     return RedirectToAction(nameof(Index));
                 }
-                catch (System.Exception ex)
+                catch (InvalidOperationException ex)
                 {
-                    ModelState.AddModelError(ex.Data[PassItemRepository.ItemPropertyKey]?.ToString(), $"Unable to save changes. {ex.Message}.");
+                    ModelState.AddModelError(ex.Data[PassItemRepository.ItemPropertyKey]?.ToString() ?? "", $"Unable to save changes. {ex.Message}.");
                 }
             }
 
@@ -75,13 +76,7 @@ namespace TimManager.Areas.Identity.Controllers.PassManager
         [HttpGet]
         public async Task<IActionResult> Details(string name)
         {
-            if (name == null)
-            {
-                return NotFound();
-            }
-
-            IdentityUser user = await _userManager.GetUserAsync(HttpContext.User);
-            PassItem passItem = await _passItemRepository.GetPassItemAsync(user.Id, name);
+            PassItem passItem = await getPassItem(name);
 
             if (passItem == null)
             {
@@ -89,6 +84,62 @@ namespace TimManager.Areas.Identity.Controllers.PassManager
             }
 
             return View(passItem);
+        }
+
+        // GET: Identity/PassItems/Edit/5
+        [HttpGet]
+        public async Task<IActionResult> Edit(string name)
+        {
+            PassItem passItem = await getPassItem(name);
+
+            if (passItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(passItem);
+        }
+
+        private async Task<PassItem> getPassItem(string name)
+        {
+            if (name == null)
+            {
+                return null;
+            }
+
+            IdentityUser user = await _userManager.GetUserAsync(HttpContext.User);
+            PassItem passItem = await _passItemRepository.GetPassItemAsync(user.Id, name);
+            
+            return passItem;
+        }
+
+        // POST: Identity/PassItems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(PassItem newPassItem)
+        {
+            ModelState.Remove(nameof(newPassItem.UserId));
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    IdentityUser user = await _userManager.GetUserAsync(HttpContext.User);
+                    newPassItem.UserId = user.Id;
+                    newPassItem.User = user;
+
+                    await _passItemRepository.UpdateAsync(newPassItem); 
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (InvalidOperationException ex)
+                {
+                    ModelState.AddModelError(ex.Data[PassItemRepository.ItemPropertyKey]?.ToString() ?? "", $"Unable to save changes. {ex.Message}.");
+                }         
+            }
+
+            return View(newPassItem);
         }
     }
 }
