0ea0bf4095540b994c3224d4b21e2b6fd22546e8
Timur <wargerun@gmail.com>
2020-08-23 18:19:59 +0300

    1. utils and test him 2. commented code was deleted 3. Tim.Pass.Store project deleted 4. Details view

---
+++ TimManager/Models/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TimManager.Models
+{
+    public static class Utils
+    {
+        public static bool Has(this string str) => !string.IsNullOrWhiteSpace(str);
+        
+        public static bool Has(this object str) => str != null;
+
+        public static bool Has<T>(this ICollection<T> collection) => collection.Count > 0;
+    }
+}
