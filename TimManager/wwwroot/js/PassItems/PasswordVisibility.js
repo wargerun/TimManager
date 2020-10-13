340f4b3baf1a94f43dd39dfcabaf5e5b535dfe7b
wargerun <wargerun@gmail.com>
2020-10-13 23:19:37 +0300

    refactoring js files and add git ignore

---
+++ TimManager/wwwroot/js/PassItems/PasswordVisibility.js
@@ -0,0 +1,11 @@
+﻿let inputPassword = document.getElementById("InputPassword");
+let checkboxShowPassword = document.getElementById("InputCheckboxShowPassword");
+
+function showPassword() {
+    if (checkboxShowPassword.checked) {
+        inputPassword.type = "text";
+    }
+    else {
+        inputPassword.type = "password";
+    }
+}
