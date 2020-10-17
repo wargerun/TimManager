43bab1afc9ceb0ee1edb0f1d51758f6bec658a91
wargerun <wargerun@gmail.com>
2020-10-18 00:27:44 +0300

    основная логика выбора лог записи таблица в интерфейсе подсчет кол-во типов логов

--- TimManager/Models/LogViewer/LogViewerViewModel.cs
+++ TimManager/Models/LogViewer/LogViewerViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using TimManager.LogViewer;
 
 namespace TimManager.Models.LogViewer
 {
@@ -6,9 +8,20 @@ namespace TimManager.Models.LogViewer
     {
         public ICollection<LogItem> LogItems { get; }
 
+        public int LogItemsCount => LogItems.Count;
+        
+        public int LogItemsUndefinedCount => LogItems.Count(l => l.LogEvent == LogEvent.Undefined);
+        public int LogItemsTraceCount => LogItems.Count(l => l.LogEvent == LogEvent.Trace);
+        public int LogItemsDebugCount => LogItems.Count(l => l.LogEvent == LogEvent.Debug);
+        public int LogItemsInfoCount => LogItems.Count(l => l.LogEvent == LogEvent.Info);
+        public int LogItemsWarnCount => LogItems.Count(l => l.LogEvent == LogEvent.Warn);
+        public int LogItemsErrorCount => LogItems.Count(l => l.LogEvent == LogEvent.Error);
+        public int LogItemsFatalCount => LogItems.Count(l => l.LogEvent == LogEvent.Fatal);
+
         public LogViewerViewModel(ICollection<LogItem> logItems = null)
         {
             LogItems = logItems ?? new List<LogItem>();
+            
         }
     }
 }
