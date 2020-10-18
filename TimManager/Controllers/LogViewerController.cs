168f46555854cba53c539ace06e8211f7a9c54a7
wargerun <wargerun@gmail.com>
2020-10-18 20:01:58 +0300

    several request method

--- TimManager/Controllers/LogViewerController.cs
+++ TimManager/Controllers/LogViewerController.cs
@@ -1,47 +1,40 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TimManager.LogViewer;
+using TimManager.LogViewer.SearchLog;
 using TimManager.Models.LogViewer;
 
 namespace TimManager.Controllers
 {
     public class LogViewerController : Controller
     {
-        private readonly LogViewer _logViewer;
+        private readonly LogViewer.LogViewer _logViewer;
+        private const string REGEXP_PATTERN = "(?<DateTime>\\d*\\/\\d*\\/\\d* \\d*:\\d*:.*) \\[(?<LogEvent>.*)\\] \\[(?<ThreadName>.*)\\] (?<Callsite>.*) - (?<Message>.*)";
 
         public LogViewerController()
         {
-            _logViewer = new LogViewer();
+            _logViewer = new LogViewer.LogViewer();
         }
         
         // GET
         public IActionResult Index()
         {
-            return View(new LogViewerViewModel());
+            return View();
         }
         
         [HttpPost]
-        public async Task<IActionResult> Index(IFormFile uploadedFile)
+        public async Task<IActionResult> Build(IFormFile uploadedFile)
         {
-            var logItems = new List<LogItem>();
-
-            if (uploadedFile != null)
-            {
-                await using var openReadStream = uploadedFile.OpenReadStream();
-                using (var streamReader = new StreamReader(openReadStream))
-                {
-                    string line;
-                    while ((line = await streamReader.ReadLineAsync()) != null)
-                    {
-                        LogItem extractLine = _logViewer.ExtractLine(line);
-                        logItems.Add(extractLine);
-                    }
-                        
-                }
-            }
-
+            if (uploadedFile == null)
+                throw new ArgumentNullException(nameof(uploadedFile));
+            
+            RegexBuilderLogItem regexBuilderLogItem = new RegexBuilderLogItem(REGEXP_PATTERN);
+            var openReadStream = uploadedFile.OpenReadStream();
+            List<LogItem> logItems = await _logViewer.ExtractLines(openReadStream, regexBuilderLogItem);
+            
             return View(new LogViewerViewModel(logItems));
         }
     }
